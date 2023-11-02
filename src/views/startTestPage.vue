0257fb2dc44c3521ab327fb9814f1da0e248d96f
weizhi_xue <weizhi_xue@hotmail.com>
2023-11-02 19:57:45 +0800

    选择模式

--- src/views/startTestPage.vue
+++ src/views/startTestPage.vue
@@ -1,16 +1,20 @@
 <script>
 import RotationChart from '../components/RotationChart.vue'
+import chooseTest from "@/components/chooseTest.vue";
 export default {
     name: "startTestPage",
     components:{
-      RotationChart
+      RotationChart,
+      chooseTest
     }
 }
+
 </script>
 
 <template>
 <div>开始测试</div>
-<RotationChart></RotationChart>
+<RotationChart/>
+  <chooseTest/>
 </template>
 
 <style scoped>

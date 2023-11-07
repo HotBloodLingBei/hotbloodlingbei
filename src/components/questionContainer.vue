404d7c47a996eab8dd2a3a66c7ad58beae29b354
CharmianLee <2022302111402@whu.edu.cn>
2023-11-07 21:26:17 +0800

    Changes

--- src/components/questionContainer.vue
+++ src/components/questionContainer.vue
@@ -37,19 +37,74 @@ export default {
       {{questionsConciseVersion.questionList[this.currentQuestionIndex].description}}
     </div>
     <div>
-      <button @click="fetchChoice(0)">{{ questionsConciseVersion.optionScore[0].text }}</button>
-      <button @click="fetchChoice(1)">{{ questionsConciseVersion.optionScore[1].text }}</button>
-      <button @click="fetchChoice(2)">{{ questionsConciseVersion.optionScore[2].text }}</button>
-      <button @click="fetchChoice(3)">{{ questionsConciseVersion.optionScore[3].text }}</button>
-      <button @click="fetchChoice(4)">{{ questionsConciseVersion.optionScore[4].text }}</button>
-      <button @click="fetchChoice(5)">{{ questionsConciseVersion.optionScore[5].text }}</button>
-      <button @click="fetchChoice(6)">{{ questionsConciseVersion.optionScore[6].text }}</button>
+      <button @click="fetchChoice(0)" class = "button">{{ questionsConciseVersion.optionScore[0].text }}</button>
+      <button @click="fetchChoice(1)" class = "button">{{ questionsConciseVersion.optionScore[1].text }}</button>
+      <button @click="fetchChoice(2)" class = "button">{{ questionsConciseVersion.optionScore[2].text }}</button>
+      <button @click="fetchChoice(3)" class = "button">{{ questionsConciseVersion.optionScore[3].text }}</button>
+      <button @click="fetchChoice(4)" class = "button">{{ questionsConciseVersion.optionScore[4].text }}</button>
+      <button @click="fetchChoice(5)" class = "button">{{ questionsConciseVersion.optionScore[5].text }}</button>
+      <button @click="fetchChoice(6)" class = "button">{{ questionsConciseVersion.optionScore[6].text }}</button>
     </div>
     <button @click="lastQuestion">上一题</button>
     <button @click="nextQuestion">下一题</button>
   </div>
 </template>
 
+
 <style scoped>
+.button {
+  --color: #00A97F;
+  padding: 0.8em 1.7em;
+  background-color: transparent;
+  border-radius: .3em;
+  position: relative;
+  overflow: hidden;
+  cursor: pointer;
+  transition: .5s;
+  font-weight: 400;
+  font-size: 12px;
+  border: 1px solid;
+  font-family: inherit;
+  text-transform: uppercase;
+  color: var(--color);
+  z-index: 1;
+}
+
+.button::before, .button::after {
+  content: '';
+  display: block;
+  width: 50px;
+  height: 50px;
+  transform: translate(-50%, -50%);
+  position: absolute;
+  border-radius: 50%;
+  z-index: -1;
+  background-color: var(--color);
+  transition: 1s ease;
+}
+
+.button::before {
+  top: -1em;
+  left: -1em;
+}
+
+.button::after {
+  left: calc(100% + 1em);
+  top: calc(100% + 1em);
+}
+
+.button:hover::before, .button:hover::after {
+  height: 410px;
+  width: 410px;
+}
+
+.button:hover {
+  color: rgb(10, 25, 30);
+}
+
+.button:active {
+  filter: brightness(.8);
+}
+
 
 </style>

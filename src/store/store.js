2919446b33d0ead6664efb9a0bed14d5acfc2ee4
luda <3135287831@qq.com>
2023-11-11 14:27:49 +0800

    调整了首页的比例大小，在assets目录下新增了16种人格svg矢量图

--- src/store/store.js
+++ src/store/store.js
@@ -2,8 +2,10 @@
 import { reactive } from 'vue'
 
 export default reactive({
-    testType:"",//conciseVersion 或者 accurateVersion
-    Scores:[],//{dimension:'I/E',value:0,valid:0}
-    initialIdx:0
-}
+        testType: "",//conciseVersion 或者 accurateVersion
+        Scores: [],//{dimension:'I/E',value:0,valid:0}
+        initialIdx: 0,
+        mbtiType: "",//最终的mbti人格类型
+
+    }
 )
--- src/views/resultPage.vue
+++ src/views/resultPage.vue
@@ -1,12 +1,22 @@
 <script>
+// import store from "@/store/store.js";
 import screenshotButton from "@/components/screenshot-button.vue";
 import learnMoreButton from "@/components/learn-more-button.vue";
 import restartTestButton from "@/components/restart-test-button.vue";
 import shareButton from "@/components/share-button.vue";
+
 export default {
     name: "resultPage",
-    components:{
-      screenshotButton,
+    data() {
+        return {
+            // mbtiType:store.mbtiType,
+            mbtiType: "intj",
+            mbtiDescription: {},
+            mbtiPicture: require("@/assets/intj-architect.svg")
+        }
+    },
+    components: {
+        screenshotButton,
         learnMoreButton,
         restartTestButton,
         shareButton
@@ -15,20 +25,34 @@ export default {
 </script>
 
 <template>
-    <div class="ButtonContainer">
-        <share-button>分享</share-button>
-        <screenshot-button>截图</screenshot-button>
-        <learn-more-button>了解更多</learn-more-button>
-        <restart-test-button>重新测试</restart-test-button>
+    <div class="contentContainer">
+        <img class="mbtiImage" :src="mbtiPicture" alt="">
+        <p class="resultTitle">你的MBTI类型是：{{ mbtiType }}</p>
+        <div class="mbtidescription">{{ mbtiDescription }}</div>
+        <div class="ButtonContainer">
+            <share-button>分享</share-button>
+            <screenshot-button>截图</screenshot-button>
+            <learn-more-button>了解更多</learn-more-button>
+            <restart-test-button>重新测试</restart-test-button>
+        </div>
     </div>
 </template>
 
 <style scoped>
-.ButtonContainer{
-    display: flex;
-    justify-content: space-between;
+.contentContainer {
+//display: flex; //justify-content: center;
+}
+
+.ButtonContainer {
+//height: 100px; //width: 100%; display: flex; justify-content: space-between;
     padding: 0 25%;
     margin-bottom: 20px;
     margin-top: 60px;
 }
+
+.mbtiImage {
+    width: 50%;
+    height: 50%;
+}
+
 </style>

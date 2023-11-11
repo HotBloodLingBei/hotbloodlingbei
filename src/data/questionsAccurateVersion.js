7b063926513d5cb4425e1ed646a96e1efc782fe0
weizhi_xue <weizhi_xue@hotmail.com>
2023-11-11 23:41:31 +0800

    加入了完整版题库

--- src/data/questionsAccurateVersion.js
+++ src/data/questionsAccurateVersion.js
@@ -1,25 +1,385 @@
 export default {
     optionScore:[
-        {text:'strongly agree', score:3},
-        {text: 'agree',score:2},
-        {text:'relatively agree', score:1},
-        {text:'neutral', score:0},
-        {text:'relatively disagree', score:-1},
-        {text: 'disagree',score: -2},
-        {text:'strongly disagree', score:-3},
+        {text:'非常同意A', score:3},
+        {text: '同意A',score:2},
+        {text:'比较同意A', score:1},
+        {text:'中立', score:0},
+        {text:'比较同意B', score:-1},
+        {text: '同意B',score: -2},
+        {text:'非常同意B', score:-3},
     ],
     questionList:[
         {
-            description: 'You find it takes effort to introduce yourself to other people.',
-            dimension:'I/E'
+            description: '3.遇到问题时，你通常喜欢_____？A.和别人讨论解决方法B.自己想办法解决',
+            dimension: 'E/I',
         },
         {
-            description: 'You find it takes effort to fall in love with other people.',
-            dimension:'I/E'
+            description: '7.当你和一群人在一起时，你常常是更愿意____？A.加入到大家的谈话中去B.独自和熟识的人交谈',
+            dimension: 'E/I',
         },
         {
-            description: 'You find it takes effort to break your heart.',
-            dimension:'I/E'
+            description: '10.当别人问你一个问题时，你经常会____？A.马上就做回答B.先在脑子里想一想',
+            dimension: 'E/I',
+        },
+        {
+            description: '19.你是一个____？A.兴趣广泛，什么都想尝试的人B.专注的投入某个兴趣的人',
+            dimension: 'E/I',
+        },
+        {
+            description: '23.新认识你的人____了解到你的兴趣所在？A.马上就能B.只有真正和你熟悉以后才能',
+            dimension: 'E/I',
+        },
+        {
+            description: '32.你更倾向或喜欢：(指你现在已具有的，而非向往的)？A.热忱B.平静',
+            dimension: 'E/I',
+        },
+        {
+            description: '62.你更倾向或喜欢：(指你现在已具有的，而非向往的)？A.开放B.私密',
+            dimension: 'E/I',
+        },
+        {
+            description: '74.和一群人在一起聚会通常会让你感到____？A.兴致勃勃B.筋疲力尽',
+            dimension: 'E/I',
+        },
+        {
+            description: '79.你通常____？A.容易和大家打成一片B.独处的时候更多',
+            dimension: 'E/I',
+        },
+        {
+            description: '81.别人____交上朋友？A.容易和你B.较难和你',
+            dimension: 'E/I',
+        },
+        {
+            description: '83.在聚会时，你____？A.说的时候多B.听的时候多',
+            dimension: 'E/I',
+        },
+        {
+            description: '13.你觉得通常别人____？A.要花很长的时间才能和你相熟B.很快就能和你熟识AB顺序互换',
+            dimension: 'E/I',
+        },
+        {
+            description: '16.更多的时候，你倾向于____？A.独处B.同他人在一起AB顺序互换',
+            dimension: 'E/I',
+        },
+        {
+            description: '26.多数人说你是一个____？A.不爱吐露心事的人B.非常坦率的人AB顺序互换',
+            dimension: 'E/I',
+        },
+        {
+            description: '38.你更倾向或喜欢：(指你现在已具有的，而非向往的)？A.缄默B.健谈AB顺序互换',
+            dimension: 'E/I',
+        },
+        {
+            description: '42.你更倾向或喜欢：(指你现在已具有的，而非向往的)？A.沉静B.活跃AB顺序互换',
+            dimension: 'E/I',
+        },
+        {
+            description: '57.你更倾向或喜欢：(指你现在已具有的，而非向往的)？A.安静的B.爱交际的AB顺序互换',
+            dimension: 'E/I',
+        },
+        {
+            description: '68.你更倾向或喜欢：(指你现在已具有的，而非向往的)？A.很少的朋友B.很多的朋友AB顺序互换',
+            dimension: 'E/I',
+        },
+        {
+            description: '77.当你一个人在家时，你____？A.能够沉静在自己的思维中B.总觉的应该做点什么事情AB顺序互换',
+            dimension: 'E/I',
+        },
+        {
+            description: '85.你____？A.只同那些兴趣相同的人才能长谈B.只要愿意，和任何人都可以长聊AB顺序互换',
+            dimension: 'E/I',
+        },
+        {
+            description: '91.在社交场合，你通常觉得____？A.很难和不认识的人进行交谈B.很容易和多数人谈笑风生AB顺序互换',
+            dimension: 'E/I',
+        },
+        {
+            description: '2.如果你是一位老师,你愿教____？A.注重实践的课程B.注重理论的课程AB顺序互换',
+            dimension: 'N/S',
+        },
+        {
+            description: '9.你希望自己被看作是一个____？A.实干家B.发明家AB顺序互换',
+            dimension: 'N/S',
+        },
+        {
+            description: '25.做许多人都做的事时，你喜欢____？A.按惯例去做B.发明自己的新方法AB顺序互换',
+            dimension: 'N/S',
+        },
+        {
+            description: '30.你更倾向或喜欢：(指你现在已具有的，而非向往的)？A.事实B.想法AB顺序互换',
+            dimension: 'N/S',
+        },
+        {
+            description: '34.你更倾向或喜欢：(指你现在已具有的，而非向往的)？A.陈述B.概念AB顺序互换',
+            dimension: 'N/S',
+        },
+        {
+            description: '39.你更倾向或喜欢：(指你现在已具有的，而非向往的)？A.常识性的B.理论性的AB顺序互换',
+            dimension: 'N/S',
+        },
+        {
+            description: '50.你更倾向或喜欢：(指你现在已具有的，而非向往的)？A.制作B.创造AB顺序互换',
+            dimension: 'N/S',
+        },
+        {
+            description: '52.你更倾向或喜欢：(指你现在已具有的，而非向往的)？A.明理的B.迷人的AB顺序互换',
+            dimension: 'N/S',
+        },
+        {
+            description: '54.你更倾向或喜欢：(指你现在已具有的，而非向往的)？A.生产B.设计AB顺序互换',
+            dimension: 'N/S',
+        },
+        {
+            description: '60.你更倾向或喜欢：(指你现在已具有的，而非向往的)？A.具体B.抽象AB顺序互换',
+            dimension: 'N/S',
+        },
+        {
+            description: '63.你更倾向或喜欢：(指你现在已具有的，而非向往的)？A.建造B.发明AB顺序互换',
+            dimension: 'N/S',
+        },
+        {
+            description: '73.你更倾向或喜欢：(指你现在已具有的，而非向往的)？A.实用B.创新AB顺序互换',
+            dimension: 'N/S',
+        },
+        {
+            description: '92.你常常是____？A.按已经有效的方法做事B.尝试一下有没有更好的办法AB顺序互换',
+            dimension: 'N/S',
+        },
+        {
+            description: '5.你通常和____相处得更好？A.喜欢想象的人B.注重现实的人',
+            dimension: 'N/S',
+        },
+        {
+            description: '11.你喜欢与____打交道？A.常有出人意料想法的人B.按照常理行事的人',
+            dimension: 'N/S',
+        },
+        {
+            description: '18.你更愿把____作为朋友？A.总能有新想法的人B.脚踏实地的人',
+            dimension: 'N/S',
+        },
+        {
+            description: '22.当你为了消遣而阅读时，你____？A.欣赏作者奇特、独创的表达B.喜欢作者的表达直接、明确',
+            dimension: 'N/S',
+        },
+        {
+            description: '27.你更倾向或喜欢：(指你现在已具有的，而非向往的)？A.看不见的B.看的见的',
+            dimension: 'N/S',
+        },
+        {
+            description: '44.你更倾向或喜欢：(指你现在已具有的，而非向往的)？A.理论性B.确定性',
+            dimension: 'N/S',
+        },
+        {
+            description: '46.你更倾向或喜欢：(指你现在已具有的，而非向往的)？A.理想B.现实',
+            dimension: 'N/S',
+        },
+        {
+            description: '48.你更倾向或喜欢：(指你现在已具有的，而非向往的)？A.想象中的B.事实上的',
+            dimension: 'N/S',
+        },
+        {
+            description: '65.你更倾向或喜欢：(指你现在已具有的，而非向往的)？A.想象B.现实',
+            dimension: 'N/S',
+        },
+        {
+            description: '67.你更倾向或喜欢：(指你现在已具有的，而非向往的)？A.理论B.事实',
+            dimension: 'N/S',
+        },
+        {
+            description: '69.你更倾向或喜欢：(指你现在已具有的，而非向往的)？A.可能B.确知',
+            dimension: 'N/S',
+        },
+        {
+            description: '71.你更倾向或喜欢：(指你现在已具有的，而非向往的)？A.新异的B.已知的',
+            dimension: 'N/S',
+        },
+        {
+            description: '82.你通常喜欢上____的课程？A.探讨理论和概念B.列举事实和图表',
+            dimension: 'N/S',
+        },
+        {
+            description: '31.你更倾向或喜欢：(指你现在已具有的，而非向往的)？A.思维B.情感AB顺序互换',
+            dimension: 'F/T',
+        },
+        {
+            description: '33.你更倾向或喜欢：(指你现在已具有的，而非向往的)？A.说服B.打动AB顺序互换',
+            dimension: 'F/T',
+        },
+        {
+            description: '35.你更倾向或喜欢：(指你现在已具有的，而非向往的)？A.分析B.同情AB顺序互换',
+            dimension: 'F/T',
+        },
+        {
+            description: '43.你更倾向或喜欢：(指你现在已具有的，而非向往的)？A.利益B.祝福AB顺序互换',
+            dimension: 'F/T',
+        },
+        {
+            description: '45.你更倾向或喜欢：(指你现在已具有的，而非向往的)？A.坚定的B.忠诚的AB顺序互换',
+            dimension: 'F/T',
+        },
+        {
+            description: '47.你更倾向或喜欢：(指你现在已具有的，而非向往的)？A.雄心B.柔肠AB顺序互换',
+            dimension: 'F/T',
+        },
+        {
+            description: '49.你更倾向或喜欢：(指你现在已具有的，而非向往的)？A.冷静的B.激情的AB顺序互换',
+            dimension: 'F/T',
+        },
+        {
+            description: '56.你更倾向或喜欢：(指你现在已具有的，而非向往的)？A.公正的B.体谅的AB顺序互换',
+            dimension: 'F/T',
+        },
+        {
+            description: '58.你更倾向或喜欢：(指你现在已具有的，而非向往的)？A.理性B.感性AB顺序互换',
+            dimension: 'F/T',
+        },
+        {
+            description: '61.你更倾向或喜欢：(指你现在已具有的，而非向往的)？A.能干的B.细腻的AB顺序互换',
+            dimension: 'F/T',
+        },
+        {
+            description: '66.你更倾向或喜欢：(指你现在已具有的，而非向往的)？A.好胜的B.好心的AB顺序互换',
+            dimension: 'F/T',
+        },
+        {
+            description: '75.你在做一个决定时，更多地会____？A.权衡实际的得失B.考虑其他人的感受AB顺序互换',
+            dimension: 'F/T',
+        },
+        {
+            description: '87.哪一个对你来说是更高的评价？A.有好胜心的B.有同情心的AB顺序互换',
+            dimension: 'F/T',
+        },
+        {
+            description: '6.你更多时候是____？A.让情感驾驭理智B.让理智驾驭情感',
+            dimension: 'F/T',
+        },
+        {
+            description: '15.下列哪一个评价更适合你？A.性情中人B.理智的人',
+            dimension: 'F/T',
+        },
+        {
+            description: '21.你更接受____？A.以情动人B.以理服人',
+            dimension: 'F/T',
+        },
+        {
+            description: '29.你更倾向或喜欢：(指你现在已具有的，而非向往的)？A.温情B.坚定',
+            dimension: 'F/T',
+        },
+        {
+            description: '37.你更倾向或喜欢：(指你现在已具有的，而非向往的)？A.敏感B.精确',
+            dimension: 'F/T',
+        },
+        {
+            description: '40.你更倾向或喜欢：(指你现在已具有的，而非向往的)？A.侠肝义胆B.深谋远虑',
+            dimension: 'F/T',
+        },
+        {
+            description: '51.你更倾向或喜欢：(指你现在已具有的，而非向往的)？A.热情的B.中立的',
+            dimension: 'F/T',
+        },
+        {
+            description: '53.你更倾向或喜欢：(指你现在已具有的，而非向往的)？A.有同情心B.有逻辑头脑',
+            dimension: 'F/T',
+        },
+        {
+            description: '70.你更倾向或喜欢：(指你现在已具有的，而非向往的)？A.宽容的B.坚决的',
+            dimension: 'F/T',
+        },
+        {
+            description: '72.你更倾向或喜欢：(指你现在已具有的，而非向往的)？A.温柔B.力量',
+            dimension: 'F/T',
+        },
+        {
+            description: '89.你更愿在一个____的老板手下工作？A.态度亲切，但有时会感情用事B.态度严厉，但始终按逻辑办事',
+            dimension: 'F/T',
+        },
+        {
+            description: '1.当你某日想去一个地方,你会____？A.事先计划好了，然后再去B.先去，然后随机应变',
+            dimension: 'J/P',
+        },
+        {
+            description: '4.你认为____？A.很早就应该开始为聚会，约会等做准备B.不必先做准备，去了以后见机行事',
+            dimension: 'J/P',
+        },
+        {
+            description: '12.按日程表办事____？A.正合你意B.束缚了你',
+            dimension: 'J/P',
+        },
+        {
+            description: '14.为如何过周末订一个计划____？A.是有必要的B.完全没必要',
+            dimension: 'J/P',
+        },
+        {
+            description: '20.当你有一项特别的工作要做时，你喜欢先____？A.察看到工作的全貌B.找出必须要做的环节',
+            dimension: 'J/P',
+        },
+        {
+            description: '28.你更倾向或喜欢：(指你现在已具有的，而非向往的)？A.计划B.随意',
+            dimension: 'J/P',
+        },
+        {
+            description: '36.你更倾向或喜欢：(指你现在已具有的，而非向往的)？A.系统性B.随机性',
+            dimension: 'J/P',
+        },
+        {
+            description: '41.你更倾向或喜欢：(指你现在已具有的，而非向往的)？A.正式B.非正式',
+            dimension: 'J/P',
+        },
+        {
+            description: '64.你更倾向或喜欢：(指你现在已具有的，而非向往的)？A.有序的B.随便的',
+            dimension: 'J/P',
+        },
+        {
+            description: '76.通常你更喜欢____？A.提前安排好该做什么B.到时候率性而为',
+            dimension: 'J/P',
+        },
+        {
+            description: '86.当你有一个报告需要在一个星期内交出时，你____？A.常留出足够的时间并能提早完成B.常常是在最后一刻及时赶出来',
+            dimension: 'J/P',
+        },
+        {
+            description: '8.你最喜欢____做事情？A.按兴致B.按计划AB顺序互换',
+            dimension: 'J/P',
+        },
+        {
+            description: '17.在日常工作中，你更喜欢____？A.在时间紧迫的情况下，争分夺秒地工作B.做好提前量，尽早把工作做完AB顺序互换',
+            dimension: 'J/P',
+        },
+        {
+            description: '24.在旅行时，你喜欢____？A.随兴致行事B.事先知道一天中该做的事AB顺序互换',
+            dimension: 'J/P',
+        },
+        {
+            description: '55.你更倾向或喜欢：(指你现在已具有的，而非向往的)？A.冲动B.决择AB顺序互换',
+            dimension: 'J/P',
+        },
+        {
+            description: '59.你更倾向或喜欢：(指你现在已具有的，而非向往的)？A.不受限制的B.安排好的AB顺序互换',
+            dimension: 'J/P',
+        },
+        {
+            description: '78.多数情况下，你____？A.随兴致做事B.按日程表做事AB顺序互换',
+            dimension: 'J/P',
+        },
+        {
+            description: '80.你做事更倾向于____？A.等到各方面的信息都全了以后再做计划B.提前很久就定计划AB顺序互换',
+            dimension: 'J/P',
+        },
+        {
+            description: '84.你觉的自己更倾向于是一个____？A.随意的人B.有秩序的人AB顺序互换',
+            dimension: 'J/P',
+        },
+        {
+            description: '88.你觉得按日程表办事____？A.虽有必要，但不喜欢B.有帮助的，非常喜欢AB顺序互换',
+            dimension: 'J/P',
+        },
+        {
+            description: '90.在完成一项大任务时，你常常是____？A.边做边考虑下一步B.事先想好每个步骤AB顺序互换',
+            dimension: 'J/P',
+        },
+        {
+            description: '93.你更喜欢按____做事情？A.当天的感觉B.已订好的日程表AB顺序互换',
+            dimension: 'J/P',
         },
     ]
 }
--- src/views/mbtiInstructionPage.vue
+++ src/views/mbtiInstructionPage.vue
@@ -1,12 +1,12 @@
 
 <template>
   <div class="display">
-      <mbti-card class="mbti-card" v-for="choice in Choices" :key="choice.id"
-                 :Description=choice.description
-                 :image-src=choice.imageSrc
-                 :mbti-type=choice.mbtiType
-                 :detail=choice.detail>
-      </mbti-card>
+  <mbti-card v-for="choice in Choices" :key="choice.id"
+             :Description = choice.description
+             :image-src = choice.imageSrc
+             :mbti-type = choice.mbtiType
+             :detail = choice.detail>
+  </mbti-card>
   </div>
   <div class="blank"></div>
 </template>
@@ -28,118 +28,118 @@ export default {
       PersonPictureLink: PersonPictureLink,
       mbtiDetail: mbtiDetail,
       Choices: [
-          {
-              id: 1,
-              description: mbtiDescription.intjDescription.shortInstruction,
-              imageSrc: PersonPictureLink.intj,
-              mbtiType: "INTJ",
-              detail: mbtiDetail.intjDetail
-          },
-          {
-              id: 2,
-              description: mbtiDescription.intpDescription.shortInstruction,
-              imageSrc: PersonPictureLink.intp,
-              mbtiType: "INTP",
-              detail: mbtiDetail.intpDetail
-          },
-          {
-              id: 3,
-              description: mbtiDescription.entjDescription.shortInstruction,
-              imageSrc: PersonPictureLink.entj,
-              mbtiType: "ENTJ",
-              detail: mbtiDetail.entjDetail
-          },
-          {
-              id: 4,
-              description: mbtiDescription.entpDescription.shortInstruction,
-              imageSrc: PersonPictureLink.entp,
-              mbtiType: "ENTP",
-              detail: mbtiDetail.entpDetail
-          },
-          {
-              id: 5,
-              description: mbtiDescription.infjDescription.shortInstruction,
-              imageSrc: PersonPictureLink.infj,
-              mbtiType: "INFJ",
-              detail: mbtiDetail.infjDetail
-          },
-          {
-              id: 6,
-              description: mbtiDescription.infpDescription.shortInstruction,
-              imageSrc: PersonPictureLink.infp,
-              mbtiType: "INFP",
-              detail: mbtiDetail.infpDetail
-          },
-          {
-              id: 7,
-              description: mbtiDescription.enfjDescription.shortInstruction,
-              imageSrc: PersonPictureLink.enfj,
-              mbtiType: "ENFJ",
-              detail: mbtiDetail.enfjDetail
-          },
-          {
-              id: 8,
-              description: mbtiDescription.enfpDescription.shortInstruction,
-              imageSrc: PersonPictureLink.enfp,
-              mbtiType: "ENFP",
-              detail: mbtiDetail.enfpDetail
-          },
-          {
-              id: 9,
-              description: mbtiDescription.istjDescription.shortInstruction,
-              imageSrc: PersonPictureLink.istj,
-              mbtiType: "ISTJ",
-              detail: mbtiDetail.istjDetail
-          },
-          {
-              d: 10,
-              description: mbtiDescription.isfjDescription.shortInstruction,
-              imageSrc: PersonPictureLink.isfj,
-              mbtiType: "ISFJ",
-              detail: mbtiDetail.isfjDetail
-          },
-          {
-              id: 11,
-              description: mbtiDescription.estjDescription.shortInstruction,
-              imageSrc: PersonPictureLink.estj,
-              mbtiType: "ESTJ",
-              detail: mbtiDetail.estjDetail
-          },
-          {
-              id: 12,
-              description: mbtiDescription.esfjDescription.shortInstruction,
-              imageSrc: PersonPictureLink.esfj,
-              mbtiType: "ESFJ",
-              detail: mbtiDetail.esfjDetail
-          },
-          {
-              id: 13,
-              description: mbtiDescription.esfpDescription.shortInstruction,
-              imageSrc: PersonPictureLink.esfp,
-              mbtiType: "ESFP",
-              detail: mbtiDetail.esfpDetail
-          },
-          {
-              id: 14,
-              description: mbtiDescription.isfpDescription.shortInstruction,
-              imageSrc: PersonPictureLink.isfp,
-              mbtiType: "ISFP",
-              detail: mbtiDetail.isfpDetail
-          },
-          {
-              id: 15,
-              description: mbtiDescription.estpDescription.shortInstruction,
-              imageSrc: PersonPictureLink.estp,
-              mbtiType: "ESTP",
-              detail: mbtiDetail.estpDetail
-          },
-          {
-              id: 16,
-              description: mbtiDescription.esfpDescription.shortInstruction,
-              imageSrc: PersonPictureLink.esfp,
-              mbtiType: "ESFP",
-              detail: mbtiDetail.esfpDetail
-          },
+        {
+          id: 1,
+          description: mbtiDescription.intjDescription.shortInstruction,
+          imageSrc: PersonPictureLink.intj,
+          mbtiType: "INTJ",
+          detail: mbtiDetail.intjDetail
+        },
+        {
+          id: 2,
+          description: mbtiDescription.intpDescription.shortInstruction,
+          imageSrc: PersonPictureLink.intp,
+          mbtiType: "INTP",
+          detail: mbtiDetail.intpDetail
+        },
+        {
+          id: 3,
+          description: mbtiDescription.entjDescription.shortInstruction,
+          imageSrc: PersonPictureLink.entj,
+          mbtiType: "ENTJ",
+          detail: mbtiDetail.entjDetail
+        },
+        {
+          id: 4,
+          description: mbtiDescription.entpDescription.shortInstruction,
+          imageSrc: PersonPictureLink.entp,
+          mbtiType: "ENTP",
+          detail: mbtiDetail.entpDetail
+        },
+        {
+          id: 5,
+          description: mbtiDescription.infjDescription.shortInstruction,
+          imageSrc: PersonPictureLink.infj,
+          mbtiType: "INFJ",
+          detail: mbtiDetail.infjDetail
+        },
+        {
+          id: 6,
+          description: mbtiDescription.infpDescription.shortInstruction,
+          imageSrc: PersonPictureLink.infp,
+          mbtiType: "INFP",
+          detail: mbtiDetail.infpDetail
+        },
+        {
+          id: 7,
+          description: mbtiDescription.enfjDescription.shortInstruction,
+          imageSrc: PersonPictureLink.enfj,
+          mbtiType: "ENFJ",
+          detail: mbtiDetail.enfjDetail
+        },
+        {
+          id: 8,
+          description: mbtiDescription.enfpDescription.shortInstruction,
+          imageSrc: PersonPictureLink.enfp,
+          mbtiType: "ENFP",
+          detail: mbtiDetail.enfpDetail
+        },
+        {
+          id: 9,
+          description: mbtiDescription.istjDescription.shortInstruction,
+          imageSrc: PersonPictureLink.istj,
+          mbtiType: "ISTJ",
+          detail: mbtiDetail.istjDetail
+        },
+        {
+          d: 10,
+          description: mbtiDescription.isfjDescription.shortInstruction,
+          imageSrc: PersonPictureLink.isfj,
+          mbtiType: "ISFJ",
+          detail: mbtiDetail.isfjDetail
+        },
+        {
+          id: 11,
+          description: mbtiDescription.estjDescription.shortInstruction,
+          imageSrc: PersonPictureLink.estj,
+          mbtiType: "ESTJ",
+          detail: mbtiDetail.estjDetail
+        },
+        {
+          id: 12,
+          description: mbtiDescription.esfjDescription.shortInstruction,
+          imageSrc: PersonPictureLink.esfj,
+          mbtiType: "ESFJ",
+          detail: mbtiDetail.esfjDetail
+        },
+        {
+          id: 13,
+          description: mbtiDescription.esfpDescription.shortInstruction,
+          imageSrc: PersonPictureLink.esfp,
+          mbtiType: "ESFP",
+          detail: mbtiDetail.esfpDetail
+        },
+        {
+          id: 14,
+          description: mbtiDescription.isfpDescription.shortInstruction,
+          imageSrc: PersonPictureLink.isfp,
+          mbtiType: "ISFP",
+          detail: mbtiDetail.isfpDetail
+        },
+        {
+          id: 15,
+          description: mbtiDescription.estpDescription.shortInstruction,
+          imageSrc: PersonPictureLink.estp,
+          mbtiType: "ESTP",
+          detail: mbtiDetail.estpDetail
+        },
+        {
+          id: 16,
+          description: mbtiDescription.esfpDescription.shortInstruction,
+          imageSrc: PersonPictureLink.esfp,
+          mbtiType: "ESFP",
+          detail: mbtiDetail.esfpDetail
+        },
       ]
     }
   },

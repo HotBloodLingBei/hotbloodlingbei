ac699804476a74c4807bc31e1fba3bf38768f5d5
CharmianLee <2022302111402@whu.edu.cn>
2023-11-12 18:09:27 +0800

    Changes

--- src/components/MyFooter.vue
+++ src/components/MyFooter.vue
@@ -8,17 +8,18 @@
     <div class="footer-content">
       <div class="basic-info">
         <!-- 网站基本信息 -->
-        <p>网站名称</p>
-        <p>网站描述</p>
+        <p>网站名称：热血林北MBTI人格测试</p>
+        <p>网站描述：MBTI人格测试网站，帮助用户了解自己的MBTI人格类型和相关概念。</p>
       </div>
       <div class="author-info">
         <!-- 著作人信息 -->
-        <p>作者名称</p>
-        <p>联系方式</p>
+        <p>作者：热血林北小组</p>
+        <p>小组成员：陆达、薛为之、李泽粲、徐睿彰</p>
+        <p>联系方式：</p>
       </div>
       <div class="copyright-info">
         <!-- 版权信息 -->
-        <p>&copy; 2023 Your Company. 版权所有。</p>
+        <p>&copy; 2023 HotBloodLinBei. 版权所有。</p>
       </div>
     </div>
     </div>
@@ -33,7 +34,9 @@
     bottom: 0;
     width: 100%;
     position: relative;
+    font-size: 12px;
     z-index: 0;
+  text-align: left;
 }
 
 .footer-content {
@@ -45,5 +48,6 @@
 
 .basic-info, .author-info, .copyright-info {
   flex: 1;
+  margin: 0 30px;
 }
 </style>
